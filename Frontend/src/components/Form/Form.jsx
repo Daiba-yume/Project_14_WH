b72af57239025f7b932816ca1b95265590b9bbbb
daiba <daiba_b@yahoo.com>
2025-05-13 14:59:09 +0200

    Ajustements form

--- Frontend/src/components/Form/Form.jsx
+++ Frontend/src/components/Form/Form.jsx
@@ -5,13 +5,17 @@ import "./Form.scss";
 import FieldSet from "../../utils/Field/FieldSet";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { StateSelect, CitySelect } from "react-country-state-city";
+import { StateSelect } from "react-country-state-city";
 import "react-country-state-city/dist/react-country-state-city.css";
 import { useState } from "react";
 
 function Form({ onSuccess }) {
   const [dateOfBirth, setDateOfBirth] = useState(null);
   const [startDate, setStartDate] = useState(null);
+
+  const [countryid] = useState("US");
+  const [stateid, setStateid] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -20,12 +24,14 @@ function Form({ onSuccess }) {
     }
   };
   return (
-    <div className="formContainer">
-      <h1>HRnet</h1>
-      <Link to="/employeeList" className="employeeList">
-        View Current Employees
-      </Link>
-      <h2>Create Employee</h2>
+    <section className="formContainer">
+      <div className="titleContainer">
+        <h1>HRnet</h1>
+        <Link to="/employeeList" className="employeeList">
+          See All Employees
+        </Link>
+      </div>
+      <h2 className="titleEmployee">Create Employee</h2>
       <form className="form" onSubmit={handleSubmit}>
         <InputField id="firstName" label="First Name" />
         <InputField id="lastName" label="Last Name" />
@@ -55,8 +61,15 @@ function Form({ onSuccess }) {
         {/* Adress */}
         <FieldSet legend="Adress">
           <InputField id="street" label="Street" />
-          <CitySelect placeHolder="Select City" />
-          <StateSelect placeHolder="Select Sate" />
+          <InputField id="city" label="City" />
+          <StateSelect
+            countryid={countryid}
+            stateid={stateid}
+            placeHolder="Select Sate"
+            onChange={(e) => {
+              setStateid(e.id);
+            }}
+          />
           <InputField id="zip-code" label="Zip code" type="number" />
         </FieldSet>
 
@@ -76,7 +89,7 @@ function Form({ onSuccess }) {
           Save
         </button>
       </form>
-    </div>
+    </section>
   );
 }
 
