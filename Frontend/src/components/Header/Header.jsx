a65e38ed7918777ea86edfe71dfabe8d4cdb68f5
daiba <daiba_b@yahoo.com>
2025-05-20 16:56:51 +0200

    Ajustements du responsive et ajout d'un menu burger

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,24 +1,33 @@
 import { NavLink } from "react-router-dom";
+import { CiMenuBurger } from "react-icons/ci";
 import "./Header.scss";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => setIsOpen(!isOpen);
+
   return (
     <nav className="header">
       <NavLink to="/">
         <img className="logo" src="/WH_logo.png" alt="logo WealthHealth" />
       </NavLink>
-      <div className="navContainer">
-        <NavLink to="/" className="home">
+      <button className="burger" onClick={handleToggle} aria-label="navigation">
+        <CiMenuBurger />
+      </button>
+
+      <div className={`navContainer ${isOpen ? "open" : ""}`}>
+        <NavLink to="/" className="home" onClick={() => setIsOpen(false)}>
           <button>
             <span>Home</span>
           </button>
         </NavLink>
-        <NavLink to="/employee">
+        <NavLink to="/employee" onClick={() => setIsOpen(false)}>
           <button>
             <span>Form</span>
           </button>
         </NavLink>
-        <NavLink to="/employeeList">
+        <NavLink to="/employeeList" onClick={() => setIsOpen(false)}>
           <button>
             <span>All Employees</span>
           </button>
